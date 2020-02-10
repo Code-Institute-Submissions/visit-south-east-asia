25e8fb515c94466706b46b6b565d3ab3e56a884c
andershup <anders242@gmail.com>
2020-02-10 11:35:46 +0000

    sendEmailJS tested and functioning

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,15 +1,18 @@
+var companyName='Visit South East Asia';
+
 function sendMail(contactForm) {
     emailjs.send("gmail", "template_8PHabYFz", {
-        "from_name": contactForm.name.value,
-        "from_email": contactForm.emailaddress.value,
-        "request_some_info": contactForm.projectsummary.value
+       "to_name": companyName,
+        "client_name": contactForm.name.value,
+        "client_email": contactForm.emailaddress.value,
+        "how_can_we_help": contactForm.howcanwehelp.value
     })
         .then(
             function (response) {
-                console.log("SUCCEWSSSSS", response);
+                console.log("Success", response);
             },
             function (error) {
-                console.log("Faileddd", error);
+                console.log("Failed", error);
             }
         );
         
